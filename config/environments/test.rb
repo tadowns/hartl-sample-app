259cc1cb0b6654c9ea21af153b562a22a3cfc037
Ty Downs <ty@ignitewithus.com>
2015-07-12 21:34:14 -0400

    Add account activations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
